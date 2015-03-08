d95db3343dbff87ea3c295f7673d69fef9eba45a
till <tillothy@gmail.com>
2015-03-08 18:06:31 +0100

    - liquibase integration (with wrong jndi for now)

--- src/main/java/com/warrensofthought/utilityCounter/delegates/MailPictureDelegate.java
+++ src/main/java/com/warrensofthought/utilityCounter/delegates/MailPictureDelegate.java
@@ -36,5 +36,29 @@ public class MailPictureDelegate implements JavaDelegate {
         email.setMsg("This is a test mail ... :-)");
         email.addTo(propertiesHelper.getPropertyValueForKey(SEND_TO));
         email.send();
+
+
+        /**
+         *   // Create the attachment
+         EmailAttachment attachment = new EmailAttachment();
+         attachment.setPath("mypictures/john.jpg");
+         attachment.setDisposition(EmailAttachment.ATTACHMENT);
+         attachment.setDescription("Picture of John");
+         attachment.setName("John");
+
+         // Create the email message
+         MultiPartEmail email = new MultiPartEmail();
+         email.setHostName("mail.myserver.com");
+         email.addTo("jdoe@somewhere.org", "John Doe");
+         email.setFrom("me@apache.org", "Me");
+         email.setSubject("The picture");
+         email.setMsg("Here is the picture you wanted");
+
+         // add the attachment
+         email.attach(attachment);
+
+         // send the email
+         email.send();
+         */
     }
 }
